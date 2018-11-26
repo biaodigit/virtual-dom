578d317ea658232314284e849bcd38b67e8d4ac8
luowendi <15622285015@163.com>
2018-11-26 21:38:09 +0800

    update snabbdom

--- snabbdom/snabbdom.js
+++ snabbdom/snabbdom.js
@@ -1,3 +1,26 @@
+const data = [
+    {
+        name: '姓名',
+        age: '年龄',
+        address: '地址'
+    },
+    {
+        name: '张三',
+        age: '20',
+        address: '北京'
+    },
+    {
+        name: '李四',
+        age: '21',
+        address: '上海'
+    },
+    {
+        name: '王五',
+        age: '22',
+        address: '广州'
+    }
+];
+
 let snabbdom = window.snabbdom;
 
 let patch = snabbdom.init([
@@ -9,20 +32,37 @@ let patch = snabbdom.init([
 
 let h = snabbdom.h;
 let container = document.getElementById('container');
+let btn = document.getElementById('btn-change');
 
-let vnode = h('ul#list',{},[
-    h('li.item',{}, 'Item1'),
-    h('li.item',{}, 'Item2')
-]);
+let vnode;
 
-patch(container, vnode);
+function render(data) {
+    let newVnode = h('table', {}, data.map(function (item) {
+        let tds = [];
 
-document.getElementById('btn-change').addEventListener('click', function () {
-    let newvnode = h('ul#list',{},[
-        h('li.item',{}, 'Item1'),
-        h('li.item',{}, 'Itemb'),
-        h('li.item',{}, 'Item3')
-    ]);
+        for (let i in item) {
+            if (item.hasOwnProperty(i)) {
+                tds.push(h('td', {}, item[i] + ''))
+            }
+        }
 
-    patch(vnode, newvnode);
-})
+        return h('tr', {}, tds)
+    }));
+
+    if (vnode) {
+        patch(vnode, newVnode)
+    } else {
+        patch(container, newVnode)
+    }
+
+    vnode = newVnode
+}
+
+render(data);
+
+
+btn.addEventListener('click', function () {
+    data[2].age = '30';
+    data[3].address = '深圳';
+    render(data)
+});
